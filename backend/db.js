c17625c4dd8528e8f8e8f45f6078fbfa064ceaf5
ayubish <ayubish16@gmail.com>
2025-02-10 21:26:00 +0300

    no update

--- backend/db.js
+++
@@ -1,7 +0,0 @@
-import { MongoClient } from "mongodb";
-
-const uri = process.env.MONGO_URI; // Your MongoDB URI from environment variables
-const client = new MongoClient(uri);
-
-await client.connect(); // Ensure the connection is established
-export { client };
